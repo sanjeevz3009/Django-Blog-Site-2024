dea24eebd8c99f77b95afd6fee4165b36c29fd49
sanjeevz3009 <sanjeevz1717@gmail.com>
2024-10-21 16:23:32 +0100

    Blog posts using CBV instead of FBV

--- blogApp/views.py
+++ blogApp/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView
 
 
 # Create your views here.
+# This function based view is not being called anymore
+# The class based view below is being called instead
 def home(request):
     context = {"posts": Post.objects.all()}
     return render(request, "blogApp/home.html", context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blogApp/home.html"  # <app>/<model>_<viewtype>.html
+    context_object_name = "posts"
+    ordering = ["-date_posted"]
 
 
 def about(request):
